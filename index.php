7905eae88340945b3ea349fb50679d551998b3e3
premmore388 <premmore388@gmail.com>
2024-12-21 18:51:49 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prem's Clock</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Prem's Clock</h1>
+    <p><?php echo date("h.i.s A");?></p>
+</body>
+</html>
